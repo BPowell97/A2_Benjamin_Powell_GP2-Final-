572a35d9fa0c6f1f9b8ecd2a0f71da583b455189
BPowell97 <bennypowell127@gmail.com>
2018-11-11 11:21:14 +1100

    Start Up Changes + Player Shooting/Enemy hit detection

---
+++ A2_Benjamin_Powell(GP2)/Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+    public GameObject deathEffect;     //downloaded sprite effect
+
+    public void TakeDamage (int damage)  // takedamage will be accessible in bullet script
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();    // Die function
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
